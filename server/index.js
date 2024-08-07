3e8278a45cf0fe43ca7c3485e0555c2fc0fd445f
Tharun-e <tharunesw26@gmail.com>
2024-08-07 12:04:04 +0530

    ++++

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-const http = require("http")
+/*const http = require("http");
+
 
 const server = http.createServer((req,res)=>{
     if(req.method ==='GET'&&req.url === '/'){
@@ -10,5 +11,20 @@ const server = http.createServer((req,res)=>{
     else if(req.method === 'GET' && req.url === '/skct'){
         res.end("hiiii");
     }
-})
+});
+server.listen(3000);*/
+const express = require("express");
+const server = express();
+server.get('/',(req,res)=>{
+    res.send("hi");
+});
+server.get('/sam',(req,res)=>{
+    res.send("samsung");
+});
+server.get('/viv',(req,res)=>{
+    res.send("vivo");
+});
+server.get('/app',(req,res)=>{
+    res.send("apple");
+});
 server.listen(3000);
